e765b8585ccf9424f5de271d33909838736e499b
Abhishek Sharma <aatish3670@gmail.com>
2021-07-30 02:50:14 +0530

    second commit

--- JS/myjQuery.js
+++ JS/myjQuery.js
@@ -1,27 +1,41 @@
 $(() => {
-  console.log("hello to console");
-  $("p:first").click(() => {
-    console.log("clicked");
-  });
-  $("#idTag").click(() => {
-    console.log("clicked");
-  });
-  $(".classTag").click(() => {
-    console.log("clicked");
-    $(this).hide();
-  });
-  $(".tag1").on({
-    click: () => {
-      console.log("clicked");
-    },
-    mouseenter: () => {
-      console.log("mouse entered");
-    },
-  });
-//   $('#btn').click(() => {
-//       $('p').toggle(1000);
-//   })
-  $('#btn').click(() => {
-      $('p').slideToggle(1000);
-  })
+  //   console.log("hello to console");
+  //   $("p:first").click(() => {
+  //     console.log("clicked");
+  //   });
+  //   $("#idTag").click(() => {
+  //     console.log("clicked");
+  //   });
+  //   $(".classTag").click(() => {
+  //     console.log("clicked");
+  //     $(this).hide();
+  //   });
+  //   $(".tag1").on({
+  //     click: () => {
+  //       console.log("clicked");
+  //     },
+  //     mouseenter: () => {
+  //       console.log("mouse entered");
+  //     },
+  //   });
+  // //   $('#btn').click(() => {
+  // //       $('p').toggle(1000);
+  // //   })
+  //   $('#btn').click(() => {
+  //       $('p').slideToggle(1000);
+  //   })
+
+  var element = "Frontend Developer";
+  var j = 0;
+  var newElement = "";
+  typewriter();
+  function typewriter() {
+    if (j < element.length) {
+      newElement += element[j];
+      $("#typewriter").html(newElement);
+      j++;
+      setTimeout(typewriter, 50);
+    }
+  }
+  //$("#typewriter").remove();
 });
